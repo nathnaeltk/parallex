389d5d238056668a7e2ff5a740f31cf92c485a70
Nathnael Teketel <hellonathnael@gmail.com>
2023-09-02 16:45:17 +0300

    Update objects.js

--- src/World/components/objects/objects.js
+++ src/World/components/objects/objects.js
@@ -8,17 +8,17 @@ async function loadObjects() {
   let object;
 
   if (objectID == 0) {
-    const data = await loader.loadAsync('https://vivien000.github.io/trompeloeil/models/tie.glb');
+    const data = await loader.loadAsync('https://nathnaeltk.github.io/parallex/models/tie.glb');
     object = setupModel(data);
     object.scale.set(0.00002, 0.00002, 0.00002);
     object.position.set(0, 0, -0.05);
   } else if (objectID == 1) {
-    const data = await loader.loadAsync('https://vivien000.github.io/trompeloeil/models/parrot.glb');
+    const data = await loader.loadAsync('https://nathnaeltk.github.io/parallex/models/parrot.glb');
     object = setupModel(data);
     object.scale.set(0.15, 0.15, 0.15);
     object.position.set(0, 0, -0.02);
   } else {
-    const data = await loader.loadAsync('https://vivien000.github.io/trompeloeil/models/vangogh.glb');
+    const data = await loader.loadAsync('https://nathnaeltk.github.io/parallex/models/vangogh.glb');
     object = setupModel(data);
     object.scale.set(0.2, 0.2, 0.2);
     object.position.set(0, -0.3, -0.5);
